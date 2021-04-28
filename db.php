1f5438fadc1773d2d82d0860bad03ca88ff8b004
Edgarmad <edmad1396@gmail.com>
2021-04-28 15:41:35 -0500

    cambiando detalle

--- db.php
+++ db.php
@@ -1,10 +1,10 @@
 <?php
 session_start();
-$dbhost = 'database-1.claawir8rmgn.us-east-2.rds.amazonaws.com';
-$dbname = 'sistema_practicum';
-$dbuser = 'admin';
-$dbpass = 'sistemapracticum';
-$port = '3306';
+$dbhost = 'host'
+$dbname = 'nombre';
+$dbuser = 'root';
+$dbpass = '';
+$port = '';
 $connection = new mysqli($dbhost, $dbuser, $dbpass, $dbname,$port);
 if($connection->connect_error){
     die("Conexión fallida: ".$connection->connect_error);
